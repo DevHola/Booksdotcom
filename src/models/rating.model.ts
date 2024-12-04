6a100fcbecd0ae5fee9c91188ee46843dce61af5
Devhola <connectola@yahoo.com>
2024-12-04 16:10:42 +0100

    temp deployment version

--- src/models/rating.model.ts
+++ src/models/rating.model.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Schema, model, Model } from "mongoose";
+import productModel from "./product.model";
 interface IRating extends Document {
     rateNumber: Number
     review?: String
@@ -24,5 +25,18 @@ const RatingSchema = new Schema<IRating>({
     }
 
 }, { timestamps: true})
+RatingSchema.methods.addAndUpdateReview = async function (data:IRating) {
+     const rating = await RateModel.create({
+        rateNumber: data.rateNumber,
+        review: data.review,
+        product: data.product,
+        user: data.user
+     })
+     await rating.save()
+     const ratings = await RateModel.find({ product: data.product })
+     const averageRatingtotal = "add all rating number & divide by total number of ratings"
+     const update = await productModel.findByIdAndUpdate(data._id, { averageRating: averageRatingtotal })
+
+}
 const RateModel: Model<IRating> = model<IRating>('ratings', RatingSchema)
 export default RateModel
